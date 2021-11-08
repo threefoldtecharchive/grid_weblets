5bbea7408e2def7e5c2464a401d8f0e631d5df18
MohamedElmdary <engm5081@gmail.com>
2021-11-08 10:10:40 +0200

    Fixed vm weblet

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import VM from "./elements/vm/Vm.wc.svelte";
-  import K from "./elements/kubernetes/Kubernetes.wc.svelte";
 </script>
 
 <VM />
-<K />
--- src/elements/vm/Vm.wc.svelte
+++ src/elements/vm/Vm.wc.svelte
@@ -9,13 +9,15 @@
   const data = new VM();
 
   const tabs = [
-    { label: "Base", icon: "far fa-paper-plane" },
-    { label: "Envs", icon: "fas fa-code-branch" },
-    { label: "Disks", icon: "far fa-save" },
-    { label: "Configs", icon: "fas fa-cogs" },
+    { label: "Base" },
+    { label: "Envs" },
+    { label: "Disks" },
+    { label: "Configs" },
   ];
   let active: string = "Base";
   let loading = false;
+  let success = false;
+  let failed = false;
 
   // prettier-ignore
   const baseFields: IFormField[] = [
@@ -59,6 +61,8 @@
   let message: string;
   function onDeployVM() {
     loading = true;
+    success = false;
+    failed = false;
 
     function onLogInfo(msg: string) {
       if (typeof msg === "string") {
@@ -69,8 +73,8 @@
     events.addListener("logs", onLogInfo);
 
     deployVM(data)
-      .then(console.log)
-      .catch(console.log)
+      .then(() => (success = true))
+      .catch(() => (failed = true))
       .finally(() => {
         loading = false;
         events.removeListener("logs", onLogInfo);
@@ -78,213 +82,224 @@
   }
 </script>
 
-<form on:submit|preventDefault={onDeployVM} class="box">
-  <h4 class="is-size-4">Deploy a Virtual Machine</h4>
+<div style="padding: 15px;">
+  <form on:submit|preventDefault={onDeployVM} class="box">
+    <h4 class="is-size-4">Deploy a Virtual Machine</h4>
+    <hr />
 
-  <div class="tabs is-centered is-boxed is-medium">
-    <ul>
-      {#each tabs as tab (tab.label)}
-        <li class={active === tab.label ? "is-active" : ""}>
-          <a href="#!" on:click|preventDefault={() => (active = tab.label)}>
-            <span class="icon is-small">
-              <i class={tab.icon} aria-hidden="true" />
-            </span>
-            <span>{tab.label}</span>
-          </a>
-        </li>
-      {/each}
-    </ul>
-  </div>
+    {#if loading}
+      <div style="text-align: center;">
+        {#if message}
+          *{message}.
+        {:else}
+          *Loading...
+        {/if}
+      </div>
+    {:else if success}
+      <div style="text-align: center;">*Successfully deployed vm.</div>
+    {:else if failed}
+      <div style="text-align: center;">*Failed to deploy vm.</div>
+    {:else}
+      <div class="tabs is-centered">
+        <ul>
+          {#each tabs as tab (tab.label)}
+            <li class={active === tab.label ? "is-active" : ""}>
+              <a href="#!" on:click|preventDefault={() => (active = tab.label)}>
+                <span>{tab.label}</span>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
 
-  {#if active === "Base"}
-    <!-- Show Base Info -->
-    {#each baseFields as field (field.symbol)}
-      <div class="field">
-        <p class="label">{field.label}</p>
-        <div class="control">
-          {#if field.type === "number"}
-            <input
-              class="input"
-              type="number"
-              placeholder={field.placeholder}
-              bind:value={data[field.symbol]}
-            />
-          {:else if field.type === "checkbox"}
-            <label class="checkbox">
+      {#if active === "Base"}
+        <!-- Show Base Info -->
+        {#each baseFields as field (field.symbol)}
+          <div class="field">
+            <p class="label">{field.label}</p>
+            <div class="control">
+              {#if field.type === "number"}
+                <input
+                  class="input"
+                  type="number"
+                  placeholder={field.placeholder}
+                  bind:value={data[field.symbol]}
+                />
+              {:else if field.type === "checkbox"}
+                <label class="checkbox">
+                  <input
+                    type="checkbox"
+                    checked={data[field.symbol]}
+                    on:change={() => (data[field.symbol] = !data[field.symbol])}
+                  />
+                  {field.label}
+                </label>
+              {:else}
+                <input
+                  class="input"
+                  type="text"
+                  placeholder={field.placeholder}
+                  bind:value={data[field.symbol]}
+                />
+              {/if}
+            </div>
+          </div>
+        {/each}
+
+        <!-- Network info -->
+        {#each networkFields as field (field.symbol)}
+          <div class="field">
+            <p class="label">{field.label}</p>
+            <div class="control">
               <input
-                type="checkbox"
-                checked={data[field.symbol]}
-                on:change={() => (data[field.symbol] = !data[field.symbol])}
+                class="input"
+                type="text"
+                placeholder={field.placeholder}
+                bind:value={data.network[field.symbol]}
               />
-              {field.label}
-            </label>
-          {:else}
-            <input
-              class="input"
-              type="text"
-              placeholder={field.placeholder}
-              bind:value={data[field.symbol]}
-            />
-          {/if}
-        </div>
-      </div>
-    {/each}
-
-    <!-- Network info -->
-    {#each networkFields as field (field.symbol)}
-      <div class="field">
-        <p class="label">{field.label}</p>
-        <div class="control">
-          <input
-            class="input"
-            type="text"
-            placeholder={field.placeholder}
-            bind:value={data.network[field.symbol]}
-          />
-        </div>
-      </div>
-    {/each}
-  {/if}
-
-  {#if active === "Envs"}
-    <div class="actions" style="margin-bottom: 20px;">
-      <button
-        type="button"
-        class="button is-primary is-light"
-        on:click={() => (data.envs = [...data.envs, new Env()])}
-      >
-        <span class="icon is-medium">
-          <i class="far fa-plus-square" />
-        </span>
-        <span>ADD Env</span>
-      </button>
-    </div>
-    <div class="vm-container">
-      {#each data.envs as env, index (env.id)}
-        <div class="box">
-          <div class="vm-header">
-            <p class="is-size-5 has-text-weight-bold">{env.key}</p>
-            <button
-              type="button"
-              class="button is-danger is-outlined"
-              on:click={() =>
-                (data.envs = data.envs.filter((_, i) => index !== i))}
-            >
-              <span>Delete</span>
-              <span class="icon is-small">
-                <i class="fas fa-times" />
-              </span>
-            </button>
+            </div>
           </div>
-          {#each envFields as field (field.symbol)}
-            <div class="field">
-              <p class="label">{field.label}</p>
-              <div class="control">
+        {/each}
+      {/if}
+
+      {#if active === "Envs"}
+        <div class="actions" style="margin-bottom: 20px;">
+          <button
+            type="button"
+            class="button is-primary is-light"
+            on:click={() => (data.envs = [...data.envs, new Env()])}
+          >
+            <span>+ ADD Env</span>
+          </button>
+        </div>
+        <div class="vm-container">
+          {#each data.envs as env, index (env.id)}
+            <div class="box">
+              <div class="vm-header">
+                <p class="is-size-5 has-text-weight-bold">{env.key}</p>
+                <button
+                  type="button"
+                  class="button is-danger is-outlined"
+                  on:click={() =>
+                    (data.envs = data.envs.filter((_, i) => index !== i))}
+                >
+                  <span>Delete</span>
+                </button>
+              </div>
+              {#each envFields as field (field.symbol)}
+                <div class="field">
+                  <p class="label">{field.label}</p>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      placeholder={field.placeholder}
+                      bind:value={env[field.symbol]}
+                    />
+                  </div>
+                </div>
+              {/each}
+            </div>
+          {/each}
+        </div>
+      {/if}
+
+      {#if active === "Disks"}
+        <div class="actions" style="margin-bottom: 20px;">
+          <button
+            type="button"
+            class="button is-primary is-light"
+            on:click={() => (data.disks = [...data.disks, new Disk()])}
+          >
+            <span class="icon is-medium">
+              <i class="far fa-plus-square" />
+            </span>
+            <span>+ ADD Disk</span>
+          </button>
+        </div>
+        <div class="vm-container">
+          {#each data.disks as disk, index (disk.id)}
+            <div class="box">
+              <div class="vm-header">
+                <p class="is-size-5 has-text-weight-bold">{disk.name}</p>
+                <button
+                  type="button"
+                  class="button is-danger is-outlined"
+                  on:click={() =>
+                    (data.disks = data.disks.filter((_, i) => index !== i))}
+                >
+                  <span>Delete</span>
+                </button>
+              </div>
+              {#each diskFields as field (field.symbol)}
+                <div class="field">
+                  <p class="label">{field.label}</p>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      placeholder={field.placeholder}
+                      bind:value={disk[field.symbol]}
+                    />
+                  </div>
+                </div>
+              {/each}
+            </div>
+          {/each}
+        </div>
+      {/if}
+
+      {#if active === "Configs"}
+        {#each configFields as field (field.symbol)}
+          <div class="field">
+            <p class="label">{field.label}</p>
+            <div class="control">
+              {#if field.type === "number"}
+                <input
+                  class="input"
+                  type="number"
+                  placeholder={field.placeholder}
+                  bind:value={data.configs[field.symbol]}
+                />
+              {:else}
                 <input
                   class="input"
                   type="text"
                   placeholder={field.placeholder}
-                  bind:value={env[field.symbol]}
+                  bind:value={data.configs[field.symbol]}
                 />
-              </div>
+              {/if}
             </div>
-          {/each}
-        </div>
-      {/each}
-    </div>
-  {/if}
-
-  {#if active === "Disks"}
-    <div class="actions" style="margin-bottom: 20px;">
-      <button
-        type="button"
-        class="button is-primary is-light"
-        on:click={() => (data.disks = [...data.disks, new Disk()])}
-      >
-        <span class="icon is-medium">
-          <i class="far fa-plus-square" />
-        </span>
-        <span>ADD Disk</span>
-      </button>
-    </div>
-    <div class="vm-container">
-      {#each data.disks as disk, index (disk.id)}
-        <div class="box">
-          <div class="vm-header">
-            <p class="is-size-5 has-text-weight-bold">{disk.name}</p>
-            <button
-              type="button"
-              class="button is-danger is-outlined"
-              on:click={() =>
-                (data.disks = data.disks.filter((_, i) => index !== i))}
-            >
-              <span>Delete</span>
-              <span class="icon is-small">
-                <i class="fas fa-times" />
-              </span>
-            </button>
           </div>
-          {#each diskFields as field (field.symbol)}
-            <div class="field">
-              <p class="label">{field.label}</p>
-              <div class="control">
-                <input
-                  class="input"
-                  type="text"
-                  placeholder={field.placeholder}
-                  bind:value={disk[field.symbol]}
-                />
-              </div>
-            </div>
-          {/each}
-        </div>
-      {/each}
-    </div>
-  {/if}
-
-  {#if active === "Configs"}
-    {#each configFields as field (field.symbol)}
-      <div class="field">
-        <p class="label">{field.label}</p>
-        <div class="control">
-          {#if field.type === "number"}
-            <input
-              class="input"
-              type="number"
-              placeholder={field.placeholder}
-              bind:value={data.configs[field.symbol]}
-            />
-          {:else}
-            <input
-              class="input"
-              type="text"
-              placeholder={field.placeholder}
-              bind:value={data.configs[field.symbol]}
-            />
-          {/if}
-        </div>
-      </div>
-    {/each}
-  {/if}
-
-  <div class="actions">
-    {#if loading && message}
-      <div class="mr-5">
-        *{message}.
-      </div>
+        {/each}
+      {/if}
     {/if}
-    <button
-      class={"button is-primary " + (loading ? "is-loading" : "")}
-      type="submit"
-      disabled={loading || !data.valid}
-    >
-      Deploy
-    </button>
-  </div>
-</form>
+
+    <div class="actions">
+      <button
+        class={"button is-primary " + (loading ? "is-loading" : "")}
+        type="submit"
+        disabled={(loading || !data.valid) && !(success || failed)}
+        on:click={(e) => {
+          if (success || failed) {
+            e.preventDefault();
+            success = false;
+            failed = false;
+            loading = false;
+          }
+        }}
+      >
+        {#if success || failed}
+          Back
+        {:else}
+          Deploy
+        {/if}
+      </button>
+    </div>
+  </form>
+</div>
 
 <style lang="scss" scoped>
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css");
   @import url("https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css");
 
   .actions {
