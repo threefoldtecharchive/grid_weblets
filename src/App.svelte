73b0d49f722354f74264abe48432bbbe3910f3cc
MohamedElmdary <engm5081@gmail.com>
2021-11-14 12:44:37 +0200

    Added farming calc component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  import L from "./elements/DeployedList/DeployedList.wc.svelte";
+  import TFFarmingCalculator from "./elements/FarmingCalculator/FarmingCalculator.wc.svelte";
+  // import L from "./elements/DeployedList/DeployedList.wc.svelte";
   // import K from "./elements/kubernetes/Kubernetes.wc.svelte";
   // import C from "./elements/caprover/Caprover.wc.svelte";
   // import V from "./elements/vm/Vm.wc.svelte";
 </script>
 
-<L />
+<TFFarmingCalculator />
 <!-- <K />
 <C />
 <V /> -->
---
+++ src/elements/FarmingCalculator/index.ts
@@ -0,0 +1,4 @@
+import FarmingCalculator from "./FarmingCalculator.wc.svelte";
+import defineElement from "../../utils/defineElement";
+
+defineElement("farming-calculator", FarmingCalculator);
