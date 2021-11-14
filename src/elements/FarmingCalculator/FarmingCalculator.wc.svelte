d9813a05a0ea29e2ae427cf976f6212bb8bd01d5
MohamedElmdary <engm5081@gmail.com>
2021-11-14 13:17:56 +0200

    Added FarmingProfile class

---
+++ src/elements/FarmingCalculator/FarmingCalculator.wc.svelte
@@ -0,0 +1,21 @@
+<svelte:options tag={null} />
+
+<script lang="ts">
+</script>
+
+<section class="farming-container">
+  <div class="box">
+    <h4 class="is-size-4 mb-4">Farming Calculator</h4>
+    <hr />
+
+    <div class="farming-content" />
+  </div>
+</section>
+
+<style lang="scss" scoped>
+  @import url("https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css");
+
+  .farming-container {
+    padding: 15px;
+  }
+</style>
