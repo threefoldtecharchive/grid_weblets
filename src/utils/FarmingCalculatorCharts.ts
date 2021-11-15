fb95d0809a3f938dd9b9f69d42a31be163f3b71b
MohamedElmdary <engm5081@gmail.com>
2021-11-15 13:07:31 +0200

    Fixed charts

--- src/utils/FarmingCalculatorCharts.ts
+++ src/utils/FarmingCalculatorCharts.ts
@@ -43,24 +43,30 @@ export function buildStackedBarChart(canvas: HTMLCanvasElement) {
       labels: ["CU", "SU", "NU", "Total"],
       datasets: [
         {
-          label: "Farming Reward Data",
-          data: [5, 7, 12, 5 + 7 + 12],
-          backgroundColor: [
-            "rgb(255, 99, 132)",
-            "rgb(54, 162, 235)",
-            "rgb(255, 205, 86)",
-          ],
+          label: "Farming Reward",
+          data: [1, 1, 1, 1],
+          backgroundColor: "rgb(255, 99, 132)",
         },
         {
-          label: "Farming Reward Data in addition to 1000 Resource",
-          data: [5, 7, 12, 5 + 7 + 12],
+          label: "Farming Reward With Extra Resources",
+          data: [2, 2, 2, 2],
+          backgroundColor: "rgb(54, 162, 235)",
+        },
+        {
+          label: "Farming Reward In 5 Years",
+          data: [3, 3, 3, 3],
           backgroundColor: "rgb(255, 205, 86)",
+        },
+        {
+          label: "Farming Reward In 5 Years With Extra Resources",
+          data: [4, 4, 4, 18],
+          backgroundColor: "rgb(63, 81, 181)",
         },
       ],
     },
     options: {
       scales: {
-        y: {
+        x: {
           beginAtZero: true,
         },
       },
