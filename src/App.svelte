975db33a37dcd364b66c3bf6c43deadb664ddf36
MohamedElmdary <engm5081@gmail.com>
2021-11-18 12:29:02 +0200

    Added profiles wc

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import Base from "./elements/base/Base.wc.svelte";
-  import VM from "./elements/vm/Vm.wc.svelte";
-  import K8S from "./elements/kubernetes/Kubernetes.wc.svelte";
-  import CapRover from "./elements/caprover/Caprover.wc.svelte";
-  import DL from "./elements/DeployedList/DeployedList.wc.svelte";
+  // import VM from "./elements/vm/Vm.wc.svelte";
+  // import K8S from "./elements/kubernetes/Kubernetes.wc.svelte";
+  // import CapRover from "./elements/caprover/Caprover.wc.svelte";
+  // import DL from "./elements/DeployedList/DeployedList.wc.svelte";
+  import Profiles from "./elements/profiles/Profiles.wc.svelte";
 </script>
 
 <Base />
 
-<VM />
+<Profiles />
+
+<!-- <VM />
 <K8S />
 <CapRover />
 <DL />
-<DL tab="k8s" />
+<DL tab="k8s" /> -->
