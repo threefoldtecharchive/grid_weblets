b2f4a66695f1b186a3d541267e9aad9a66c25e4c
MohamedElmdary <engm5081@gmail.com>
2021-11-21 16:48:07 +0200

    Added new build for realse

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import Base from "./elements/base/Base.wc.svelte";
   import Profiles from "./elements/profiles/Profiles.wc.svelte";
   // import K8S from "./elements/kubernetes/Kubernetes.wc.svelte";
-  // import VM from "./elements/vm/Vm.wc.svelte";
+  import VM from "./elements/vm/Vm.wc.svelte";
   // import CapRover from "./elements/caprover/Caprover.wc.svelte";
   // import DL from "./elements/DeployedList/DeployedList.wc.svelte";
 </script>
@@ -12,6 +12,8 @@
 
 <Base />
 <Profiles />
+
+<VM />
 <!-- <DL /> -->
 
 <!-- <C /> -->
