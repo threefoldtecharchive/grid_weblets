2727009ef3b74b46503ff8740bffb86ab79afc79
MohamedElmdary <engm5081@gmail.com>
2021-11-22 13:08:28 +0200

    Fixed farming calc labels

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import Base from "./elements/base/Base.wc.svelte";
-  import Profiles from "./elements/profiles/Profiles.wc.svelte";
-  import DL from "./elements/DeployedList/DeployedList.wc.svelte";
+  // import Base from "./elements/base/Base.wc.svelte";
+  // import Profiles from "./elements/profiles/Profiles.wc.svelte";
+  // import DL from "./elements/DeployedList/DeployedList.wc.svelte";
+  import C from "./elements/FarmingCalculator/FarmingCalculator.wc.svelte";
 </script>
 
-<Base />
+<C />
+<!-- <Base />
 <Profiles />
-<DL />
+<DL /> -->
