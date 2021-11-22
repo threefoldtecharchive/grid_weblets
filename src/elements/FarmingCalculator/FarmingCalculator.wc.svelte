a525ddf6e680921fced4e2ec8f0aef90c9bb72f9
MohamedElmdary <engm5081@gmail.com>
2021-11-22 09:30:36 +0200

    Added Switch toggle on ROI

--- src/elements/FarmingCalculator/FarmingCalculator.wc.svelte
+++ src/elements/FarmingCalculator/FarmingCalculator.wc.svelte
@@ -264,18 +264,27 @@
                 <canvas bind:this={pieCanvas} />
               </div>
               <div class="chart chart-2">
-                <label style="display: flex; align-items: center;">
-                  <input
-                    class="mr-2 ml-2"
-                    type="checkbox"
-                    checked={showChartRoi}
-                    on:change={() => {
-                      showChartRoi = !showChartRoi;
-                      _updateLineCanvas();
-                    }}
-                  />
-                  <p class="label">Return On Investment</p>
-                </label>
+                <div style="display: flex; align-items: center;">
+                  <label class="switch">
+                    <input
+                      type="checkbox"
+                      checked={showChartRoi}
+                      id="roi-checkbox"
+                      on:change={() => {
+                        showChartRoi = !showChartRoi;
+                        _updateLineCanvas();
+                      }}
+                    />
+                    <span class="slider" />
+                  </label>
+                  <label
+                    for="roi-checkbox"
+                    class="label ml-2"
+                    style="cursor: pointer;"
+                  >
+                    Return On Investment
+                  </label>
+                </div>
                 <canvas bind:this={lineCanvas} />
               </div>
             </div>
@@ -473,6 +482,60 @@
 
     &-3 > div {
       width: calc(100% / 3);
+    }
+  }
+
+  /* Switch */
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+
+    input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+    }
+
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+      border-radius: 34px;
+    }
+
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 26px;
+      width: 26px;
+      left: 4px;
+      bottom: 4px;
+      background-color: white;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+      border-radius: 50%;
+    }
+
+    input:checked + .slider {
+      background-color: #2196f3;
+    }
+
+    input:checked + .slider {
+      box-shadow: 0 0 1px #2196f3;
+    }
+
+    input:checked + .slider:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
     }
   }
 </style>
