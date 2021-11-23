20631906b4311f68917ab0ac1cfe71873ad7f1f8
MohamedElmdary <engm5081@gmail.com>
2021-11-23 18:03:24 +0200

    Refactor K8S

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,15 @@
-<svelte:options tag="tf-app" />
-
 <script lang="ts">
-  // import Base from "./elements/base/Base.wc.svelte";
+  import Base from "./elements/base/Base.wc.svelte";
   // import Profiles from "./elements/profiles/Profiles.wc.svelte";
   // import VM from "./elements/vm/Vm.wc.svelte";
-  // import K8S from "./elements/kubernetes/Kubernetes.wc.svelte";
+  import K8S from "./elements/kubernetes/Kubernetes.wc.svelte";
   // import VM from "./types/vm";
   // import Inputs from "./components/Inputs.svelte";
   // let data = new VM();
-  import Test from "./elements/Test/Test.wc.svelte";
 </script>
 
-<Test />
-<!-- <Base />
-<K8S /> -->
+<Base />
+<K8S />
 
 <!-- <Inputs bind:data fields={[{ label: "Value", symbol: "name", type: "text" }]} />
 
