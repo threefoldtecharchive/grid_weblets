32024131124d88d313cc2dd0989fbe80c07e1b94
MohamedElmdary <engm5081@gmail.com>
2021-11-24 13:24:29 +0200

    Refactored VM

---
+++ src/components/DeleteBtn.svelte
@@ -0,0 +1,12 @@
+<svelte:options tag="tf-delete-btn" />
+
+<script lang="ts">
+  export let name: string;
+</script>
+
+<div class="is-flex is-justify-content-space-between is-align-items-center">
+  <p class="is-size-5 has-text-weight-bold">{name}</p>
+  <button type="button" class="button is-danger" on:click>
+    <span>-</span>
+  </button>
+</div>
