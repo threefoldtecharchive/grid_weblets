8f2e0bcb62826b844ea30da444b223c77bffae45
MohamedElmdary <engm5081@gmail.com>
2021-11-24 15:26:51 +0200

    Refactored profiles

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -2,19 +2,30 @@
 
 <script lang="ts">
   import type { ITab } from "../types";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher<{ removed: number }>();
 
   export let tabs: ITab[];
   export let active: string;
   export let centered: boolean = true;
+
+  const onRemove = (idx: number) => () => dispatch("removed", idx);
 </script>
 
 <div class={"tabs " + (centered ? "is-centered" : "")}>
   <ul>
     {#if tabs}
-      {#each tabs as tab (tab.label)}
+      {#each tabs as tab, index (tab.label)}
         <li class={active === tab.value ? "is-active" : ""}>
           <a href="#!" on:click|preventDefault={() => (active = tab.value)}>
             <span>{tab.label}</span>
+            {#if tab.removable}
+              <button
+                class="ml-2 is-small delete"
+                on:click|preventDefault|stopPropagation={onRemove(index)}
+              />
+            {/if}
           </a>
         </li>
       {/each}
