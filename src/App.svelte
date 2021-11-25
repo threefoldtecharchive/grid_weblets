c37397ad193a04c36eb6699dc47c6ccb3f0fe844
MohamedElmdary <engm5081@gmail.com>
2021-11-25 16:56:12 +0200

    Added validation to profiles cmp

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
   import Base from "./elements/base/Base.wc.svelte";
-  import VM from "./elements/vm/Vm.wc.svelte";
-  import Model from "./components/DeploymentModal.svelte";
-
-  const x = {} as any;
-  for (let i = 0; i < 200; i++) {
-    x["hello " + i] = "world";
-  }
+  import ProfileManager from "./elements/profiles/Profiles.wc.svelte";
 </script>
 
-<!-- <Base />
-<VM /> -->
-
-<Model data={x} />
+<Base />
+<ProfileManager />
