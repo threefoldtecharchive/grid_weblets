f930c642dfaa19d34480636fc15a0a3d1ac41079
MohamedElmdary <engm5081@gmail.com>
2021-11-30 17:25:13 +0200

    Added Notification Manager

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Base from "./elements/base/Base.wc.svelte";
   import ProfileManager from "./elements/profiles/Profiles.wc.svelte";
+  import NotificationManager from "./elements/Notification/Notification.wc.svelte";
 </script>
 
 <Base />
 <ProfileManager />
+<NotificationManager />
--- src/global.d.ts
+++ src/global.d.ts
@@ -2,18 +2,18 @@
 
 import * as client from "ts-rmb-http-client";
 import * as grid3_client from "grid3_client";
-import * as polkadot from "@polkadot/api";
+
+// stores
 import baseConfigStore from "./stores/baseConfig";
 import deploymentStore from "./stores/deploymentStore";
-import * as bip39 from "bip39";
+import notificationStore from "./stores/notifications";
 
 interface AppConfigs {
   client: typeof client;
   grid3_client: typeof grid3_client;
-  polkadot: typeof polkadot;
   baseConfig: typeof baseConfigStore;
   deploymentStore: typeof deploymentStore;
-  bip39: typeof bip39;
+  notificationStore: typeof notificationStore;
 }
 
 declare global {
