57c0c2e101fd5825d3c5819c1e789c2b2d90a532
MohamedElmdary <engm5081@gmail.com>
2021-12-01 11:51:22 +0200

    Added Validator handler in IFormField

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
-  import FarmingCalculator from "./elements/FarmingCalculator/FarmingCalculator.wc.svelte";
+  import type { IFormField } from "./types";
+  import Input from "./components/Input.svelte";
+
+  let data = "";
+  const field: IFormField = {
+    label: "Name",
+    type: "text",
+    symbol: "name",
+    placeholder: "Fill in your name",
+    tooltip: "Your Real Name",
+    // prettier-ignore
+    validator(name: string) {
+      if (!name.match(/[a-z]/)) return "Name must include at least one lower letter";
+      if (!name.match(/[A-Z]/)) return "Name must include at least one upper letter";
+      if (!name.match(/[0-9]/)) return "Name must include at least one number";
+      if (name.length < 6) return "Name must be at least 6 chars long";
+      if (name.length > 15) return "Name must be at most 15 chars long";
+    },
+  };
 </script>
 
-<FarmingCalculator />
+<div style="padding: 25px;">
+  <Input bind:data {field} />
+</div>
