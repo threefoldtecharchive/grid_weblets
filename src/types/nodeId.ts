60daba27d6729ae0290f974f631ac117188fe361
MohamedElmdary <engm5081@gmail.com>
2021-12-05 15:43:05 +0200

    Fixed NodeID selection

---
+++ src/types/nodeId.ts
@@ -0,0 +1,23 @@
+export default class NodeID {
+  constructor(
+    public type: "automatic" | "manual" = null,
+    public filters = {
+      // boolean
+      publicIPs: null, // -
+
+      // string
+      country: null,
+      farmName: null, // *
+
+      // number
+      cru: null, // *
+      mru: null, // *
+      sru: null, // *
+
+      /* updater */
+      update: (key: string, value: any) => {
+        this.filters[key] = value;
+      },
+    }
+  ) {}
+}
