9c80516d5bd53fe1097a0b3c98cfbed341b0e052
MohamedElmdary <engm5081@gmail.com>
2021-12-09 11:34:17 +0200

    Updated name validator

--- src/utils/validateName.ts
+++ src/utils/validateName.ts
@@ -1,7 +1,8 @@
+const NAME_REGEX = /^[a-z0-9]+$/i;
+
 // prettier-ignore
 export default function validateName(name: string): string | void {
-    if (!name.match(/[0-9]/g)) return "Name must include at least 1 number";
-    if (!name.match(/[a-z]/gi)) return "Name must include letters";
-    if (name.length < 6) return "Name must be at least 6 chars";
-    if (name.length > 15) return "Name must be at most 15 chars";
+    if (name.length === 0) return "Name must be at least 1 character";
+    if (!NAME_REGEX.test(name)) return "Name can only include alphanumeric characters";
+    if (name.length > 15) return "Name must be at most 15 characters";
 }
