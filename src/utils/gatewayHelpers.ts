9b75e4db94a4b1267603f1a3ae0d3fd6c4dcc9ea
Ahmed Samir <asnweery@gmail.com>
2021-12-27 15:11:14 +0200

    Remove getUniqueName

--- src/utils/gatewayHelpers.ts
+++ src/utils/gatewayHelpers.ts
@@ -14,21 +14,8 @@ export async function selectGatewayNode() {
   return [nodeId, nodeDomain];
 }
 
-export async function getUniqueName(client: any, name: string) {
-  let info = await client.gateway.getObj(name);
-
-  if (info.length != 0) {
-    const suffix = Math.floor(Math.random() * 10);
-    name += suffix;
-    return getUniqueName(client, name);
-  } else {
-    return name.toLowerCase();
-  }
-}
-
 export async function getUniqueDomainName(client, solution_type, name) {
   let twin_id = await client.twins.get_my_twin_id();
   let domainName = `${solution_type}${twin_id}${name}`;
-  domainName = await getUniqueName(client, domainName);
   return domainName;
 }
