ce89f0377d2d60a8851f0ce6e8f1b57325efda54
MohamedElmdary <engm5081@gmail.com>
2022-01-06 12:49:47 +0200

    Added new rootFs using decimal.js

--- src/utils/rootFs.ts
+++ src/utils/rootFs.ts
@@ -1,15 +1,15 @@
+import { Decimal } from "decimal.js";
+
 const GB = 1024;
 
 export default function rootFs(
   cpu_in_cores: number,
   mem_in_mb: number
 ): number {
-  const mem_in_gb = mem_in_mb / GB;
-  const x = cpu_in_cores * mem_in_gb;
-  const y = 8;
+  const cu = new Decimal(cpu_in_cores)
+    .mul(mem_in_mb)
+    .divToInt(8 * GB)
+    .toNumber();
 
-  const cu = x / y;
-
-  if (cu === 0) return 500 / GB;
-  return 2;
+  return cu === 0 ? 500 / GB : 2;
 }
