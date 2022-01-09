7ec68f7f6dc272971ccdefedf5bdb88a3607471f
MohamedElmdary <engm5081@gmail.com>
2022-01-09 15:31:38 +0200

    Fixed rootFs null error

--- src/utils/rootFs.ts
+++ src/utils/rootFs.ts
@@ -6,6 +6,9 @@ export default function rootFs(
   cpu_in_cores: number,
   mem_in_mb: number
 ): number {
+  cpu_in_cores = cpu_in_cores || 0;
+  mem_in_mb = mem_in_mb || 0;
+
   const cu = new Decimal(cpu_in_cores)
     .mul(mem_in_mb)
     .divToInt(8 * GB)
