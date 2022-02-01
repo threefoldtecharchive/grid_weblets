4a2e85f365835d994a9e400ac872dffe197fc303
MohamedElmdary <engm5081@gmail.com>
2022-02-01 16:56:36 +0200

    Allow user to edit mountpoint as needed

--- src/types/vm.ts
+++ src/types/vm.ts
@@ -20,7 +20,7 @@ export class Disk {
     { label: "Name", symbol: "name", placeholder: "Disk Name", type: "text" },
     { label: "Size", symbol: "size", placeholder: "Disk size in GB", type: "number", validator: validateDisk, invalid: false },
     {
-      label: "Mount Point", symbol: "mountpoint", placeholder: "Disk Mount Point", type: "text", disabled: true, validator(point: string): string | void {
+      label: "Mount Point", symbol: "mountpoint", placeholder: "Disk Mount Point", type: "text", validator(point: string): string | void {
         point = point.trim();
         if (point === "" || point === "/" || !point.startsWith("/")) return "Mount Point must start '/' and can't be positioned at root('/')"
       }, invalid: false
