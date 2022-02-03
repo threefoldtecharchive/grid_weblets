405b9342d267bce6977a3bd44972de4418af3205
Omarabdul3ziz <omarabdul3ziz@gmail.com>
2022-02-03 16:13:08 +0200

    fixes

--- src/elements/DeployedList/DeployedList.wc.svelte
+++ src/elements/DeployedList/DeployedList.wc.svelte
@@ -40,7 +40,6 @@
     { label: "Discourse", value: "discourse" },
     { label: "Taiga", value: "taiga" },
     { label: "Owncloud", value: "owncloud" },
-    { label: "Discourse", value: "discourse" },
     { label: "Presearch", value: "presearch" }
 
   ];
@@ -636,39 +635,6 @@
           <Alert
             type="danger"
             message={err.message || err || "Failed to list presearch instances"}
-          />
-        {/await}
-        <!-- Discourse -->
-      {:else if active === "discourse"}
-        {#await list.loadDiscourse()}
-          <Alert type="info" message="Listing discourse Instances..." />
-        {:then rows}
-          {#if rows.length}
-            <Table
-              rowsData={rows}
-              headers={_vmHeader}
-              rows={_createVMRow(rows)}
-              actions={[
-                {
-                  type: "info",
-                  label: "Show Details",
-                  click: (_, i) => (infoToShow = rows[i].details),
-                  disabled: () => removing !== null,
-                  loading: (i) => removing === rows[i].name,
-                },
-              ]}
-              on:selected={_onSelectRowHandler}
-            />
-          {:else}
-            <Alert
-              type="info"
-              message="No discourse instances found on this profile."
-            />
-          {/if}
-        {:catch err}
-          <Alert
-            type="danger"
-            message={err.message || err || "Failed to list discourse instances"}
           />
         {/await}
       {/if}
