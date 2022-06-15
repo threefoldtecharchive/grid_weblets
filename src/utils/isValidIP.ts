1ed69e2cdcb27bbc938898822965bfcc4acd6fea
zaelgohary <zainabelgohary19@gmail.com>
2022-06-15 16:27:44 +0200

    removing unneeded validation files.

--- src/utils/isValidIP.ts
+++
@@ -1,8 +0,0 @@
-export function isValidIP(ip: string) {
-  const IP_RANGE_REGEX = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,3}$/;
-  if (IP_RANGE_REGEX.test(ip)) return true;
-  else {
-    return false
-  }
-}
-
