1677f9fad21bde0e1ba891adb55ce823640c5ff5
rawdaGastan <rody.rawda.rr@gmail.com>
2022-06-15 16:32:38 +0200

    qr-code

--- src/components/QrCode.svelte
+++ src/components/QrCode.svelte
@@ -1,31 +1,16 @@
 <svelte:options tag="tf-qrcode" />
 
 <script lang="ts">
+  import QRCode from "qrcode";
   export let value: string = "";
   export let size: string = "200";
 
-  let image: string = "";
-
-  import QRCode from 'qrcode';
-
-  var opts = {
-    errorCorrectionLevel: 'H',
-    type: 'image/jpeg',
-    quality: 0.3,
-    margin: 1,
-    color: {
-        dark:"#010599FF",
-        light:"#FFBF60FF"
-    }
-  }
-
-  QRCode.toDataURL(value, opts, function (err, url) {
-    if (err) throw err
-        image = url;
-    console.log(image);
-  })
-
 </script>
 
-<img src={image} alt={value} style="size: {size};"/>
-
+{#await QRCode.toDataURL(value)}
+  <p>Generating QR Code...</p>
+{:then src}
+  <img {src} alt={value} width={size} />
+{:catch err}
+  <p>Failed to generate QR Code.</p>
+{/await}
