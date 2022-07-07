d4660d85a76cba61383f90abd3e2aefbf2c4cf73
rawdaGastan <rody.rawda.rr@gmail.com>
2022-07-07 11:35:33 +0200

    change network for every deployment

--- src/utils/createNetwork.ts
+++ src/utils/createNetwork.ts
@@ -1,7 +1,8 @@
-import type { Network } from "../types/kubernetes";
+import { Network } from "../types/kubernetes";
 const { NetworkModel } = window.configs?.grid3_client ?? {};
 
 export default function createNetwork(nw: Network, access: boolean = false) {
+  nw = new Network();
   const network = new NetworkModel();
   network.name = nw.name;
   network.ip_range = nw.ipRange;
