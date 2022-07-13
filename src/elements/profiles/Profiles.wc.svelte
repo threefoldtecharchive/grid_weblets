7941367dd41ad9af9cf00dd4e615688cf46db8e2
rawdaGastan <rody.rawda.rr@gmail.com>
2022-07-13 17:09:26 +0200

    confliction between profiles

--- src/elements/profiles/Profiles.wc.svelte
+++ src/elements/profiles/Profiles.wc.svelte
@@ -2,7 +2,7 @@
 
 <script lang="ts">
   import type { IFormField, ITab } from "../../types";
-  import type { IProfile } from "../../types/Profile";
+  import type { IProfile, IProfileErrors } from "../../types/Profile";
   import validateMnemonics from "../../utils/validateMnemonics";
   import validateProfileName, {
     isInvalid,
@@ -61,14 +61,14 @@
 
   // prettier-ignore
   const fields: IFormField[] = [
-    { label: "Profile Name", symbol: "name", placeholder: "Profile Name", validator: validateProfileName, type: "text" },
+    { label: "Profile Name", symbol: "name", placeholder: "Profile Name", type: "text" },
     // { label: "Network Environment", symbol: "networkEnv", type: "select", disabled: true, options: [
     //   { label: "Testnet", value: "test" },
     //   { label: "Devnet", value: "dev" }
     // ] },
-    { label: "Mnemonics", symbol: "mnemonics", placeholder: "Enter Your Mnemonics", validator: syncValidateMnemonics, type: "password" },
+    { label: "Mnemonics", symbol: "mnemonics", placeholder: "Enter Your Mnemonics", type: "password" },
     // { label: "TFChain Configurations Secret", symbol: "storeSecret", placeholder: "  Secret key used to encrypt your data on TFChain", type: "password" },
-    { label: "Public SSH Key", symbol: "sshKey", placeholder: "Your public SSH key will be added as default to all deployments.", validator: validateSSH, type: "text" },
+    { label: "Public SSH Key", symbol: "sshKey", placeholder: "Your public SSH key will be added as default to all deployments.", type: "text" },
   ];
 
   const twinField: IFormField = { label: "Twin ID", type: "number", symbol: "twinId", placeholder: "Loading Twin ID...", disabled: true }; // prettier-ignore
@@ -274,7 +274,11 @@
                 bind:data={activeProfile.name}
                 field={{
                   ...fields[0],
+                  error: activeProfile.name == "" ? null : validateProfileName(activeProfile.name),
                   disabled: activeProfileId === activeProfile.id,
+                }}
+                on:input={() => {
+                  fields[0].error = validateProfileName(activeProfile.name);
                 }}
               />
 
@@ -282,6 +286,7 @@
                 bind:data={activeProfile.mnemonics}
                 field={{
                   ...fields[1],
+                  error: activeProfile.mnemonics == "" ? null : syncValidateMnemonics(activeProfile.mnemonics),
                   disabled: activeProfileId === activeProfile.id,
                 }}
               />
@@ -295,6 +300,7 @@
                 bind:data={activeProfile.sshKey}
                 field={{
                   ...fields[2],
+                  error: activeProfile.sshKey == "" ? null : validateSSH(activeProfile.sshKey),
                   disabled: activeProfileId === activeProfile.id,
                 }}
               />
