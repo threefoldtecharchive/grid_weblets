ede590a1943ef6132516417b69e30035f1e455f0
Rawda Fawzy <47260239+rawdaGastan@users.noreply.github.com>
2022-07-13 12:22:17 +0200

    countries with nodes only (#901)
    * show countries with nodes only
    
    * remove async

--- src/utils/fetchFarms.ts
+++ src/utils/fetchFarms.ts
@@ -5,48 +5,39 @@ import gqlApi from "./gqlApi";
 const queryCount = `
 query GetLimits {
     farms: farmsConnection(orderBy: farmID_ASC) { farms_limit: totalCount }
-    countries: countriesConnection(orderBy: countryID_ASC) { countries_limit: totalCount }
 }
 `;
 
 const queryCountIPFilter = `
 query GetLimits {
   farms: farmsConnection(where: {publicIPs_some: {}}, orderBy: farmID_ASC) { farms_limit: totalCount }
-  countries: countriesConnection(orderBy: countryID_ASC) { countries_limit: totalCount }
-
 }
 `;
 
 interface IQueryCount {
   farms: { farms_limit: number };
-  countries: { countries_limit: number };
 }
 
 const queryData = `
-query GetData($farms_limit: Int!, $countries_limit: Int!) {
+query GetData($farms_limit: Int!) {
     farms(limit: $farms_limit) { name }
-    countries(limit: $countries_limit) { name code }
 }
 `;
 
 const queryDataIPFilter = `
-query GetData($farms_limit: Int!, $countries_limit: Int!) {
+query GetData($farms_limit: Int!) {
   farms(limit: $farms_limit, where: {publicIPs_some: {}}) {
     name
-  }
-  countries(limit: $countries_limit) {
-    name
-    code
   }
 }
 `;
 
 interface IQueryData {
   farms: Array<{ name: string }>;
-  countries: Array<{ name: string; code: string }>;
 }
 
-export default function fetchFarmAndCountries(profile: IProfile, filters: FilterOptions,) {
+
+export default function fetchFarms(profile: IProfile, filters: FilterOptions,) {
   var query = queryCount;
   var queryDataSelect = queryData;
   if(filters.publicIPs){
@@ -55,8 +46,8 @@ export default function fetchFarmAndCountries(profile: IProfile, filters: Filter
   }
     
   return gqlApi<IQueryCount>(profile, query)
-    .then(({ farms: { farms_limit }, countries: { countries_limit } }) => {
-      return { farms_limit, countries_limit };
+    .then(({ farms: { farms_limit }}) => {
+      return { farms_limit };
     })
     .then((vars) => {
       return gqlApi<IQueryData>(profile, queryDataSelect, vars);
