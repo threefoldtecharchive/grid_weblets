ad55dd22af2d69107d64926c0b079822bb29a36d
omarabdul3ziz <omarabdul3ziz@gmail.com>
2022-07-17 17:18:28 +0200

    filter farms with online nodes

--- src/utils/fetchFarmAndCountries.ts
+++ src/utils/fetchFarmAndCountries.ts
@@ -24,7 +24,7 @@ interface IQueryCount {
 
 const queryData = `
 query GetData($farms_limit: Int!, $countries_limit: Int!) {
-    farms(limit: $farms_limit) { name }
+    farms(limit: $farms_limit) { name farmID }
     countries(limit: $countries_limit) { name code }
 }
 `;
@@ -33,6 +33,7 @@ const queryDataIPFilter = `
 query GetData($farms_limit: Int!, $countries_limit: Int!) {
   farms(limit: $farms_limit, where: {publicIPs_some: {}}) {
     name
+    farmID
   }
   countries(limit: $countries_limit) {
     name
@@ -58,7 +59,25 @@ export default function fetchFarmAndCountries(profile: IProfile, filters: Filter
     .then(({ farms: { farms_limit }, countries: { countries_limit } }) => {
       return { farms_limit, countries_limit };
     })
-    .then((vars) => {
-      return gqlApi<IQueryData>(profile, queryDataSelect, vars);
+    .then(async (vars) => {
+      let { farms, countries } = await gqlApi<IQueryData>(profile, queryDataSelect, vars);
+
+      farms = await getOnlineFarms(profile, farms);
+
+      return { farms, countries };
     });
+}
+
+export async function getOnlineFarms(profile, farms) {
+  let onlineFarms = [];
+
+  for (let farm of farms) {
+    let data = await fetch(
+      `https://gridproxy.${profile.networkEnv}.grid.tf/nodes?farm_ids=${farm.farmID}&status=up`
+    ).then((response) => response.json());
+
+    if (data.length > 0) onlineFarms.push(farm);
+  }
+
+  return onlineFarms;
 }
