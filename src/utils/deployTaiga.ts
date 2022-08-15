5cb33c05952476934372630439bbe3d704005d93
omarabdul3ziz <omarabdul3ziz@gmail.com>
2022-08-15 17:14:40 +0200

    update taiga flist

--- src/utils/deployTaiga.ts
+++ src/utils/deployTaiga.ts
@@ -85,7 +85,7 @@ async function deployTaigaVM(profile: IProfile, data: Taiga) {
   vm.memory = memory;
   vm.rootfs_size = rootFs(cpu, memory);
   vm.flist =
-    "https://hub.grid.tf/tf-official-apps/grid3_taiga_docker-latest.flist";
+    "https://hub.grid.tf/omarabdulaziz.3bot/omarabdul3ziz-taiga-restart.flist";
   vm.entrypoint = "/sbin/zinit init";
   vm.env = {
     SSH_KEY: profile.sshKey,
