5fe373c5189eb201fc94867375318a74b7221b7c
MohamedElmdary <engm5081@gmail.com>
2022-08-29 14:23:23 +0200

    Remove console logs

--- src/components/SelectNodeId.svelte
+++ src/components/SelectNodeId.svelte
@@ -217,7 +217,6 @@
     symbol: "nodeId",
     type: "number",
     placeholder: "Your Node ID",
-    // validator: _nodeValidator,
   };
 
   interface IResources { cru: number; sru: number; hru: number; mru: number; ipv4u: number; } // prettier-ignore
