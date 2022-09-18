cf9fc4ef3975297465960c609c3bbb1628b23d71
zaelgohary <zainabelgohary19@gmail.com>
2022-09-18 09:48:58 +0200

    editing regex to reject special chars at the end

--- src/utils/validateName.ts
+++ src/utils/validateName.ts
@@ -19,7 +19,7 @@ const NUM_REGEX = /^[1-9](\d?|\d+)$/;
 const SSH_REGEX =
   /^(sk-)?(ssh-rsa AAAAB3NzaC1yc2|ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNT|ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzOD|ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1Mj|ssh-ed25519 AAAAC3NzaC1lZDI1NTE5|ssh-dss AAAAB3NzaC1kc3)[0-9A-Za-z+/]+[=]{0,3}( .*)?$/;
 
-const ENDPOINT_REGEX = /^(((?:wss):\/\/)[a-z0-9-.]+\.[a-z]{2,4}\/?([\w-])?)/;
+const ENDPOINT_REGEX = /^(wss:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\./\$]+[^\W\s]$/;
 
 // prettier-ignore
 export default function validateName(name: string): string | void {
