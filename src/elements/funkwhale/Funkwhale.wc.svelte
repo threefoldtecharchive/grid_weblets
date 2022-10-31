151fa5d71f1a821647e3ec90ab928c2df1d4ef1c
MohamedElmdary <engm5081@gmail.com>
2022-10-31 14:49:13 +0200

    Fix SelectGateway & LoadGatewayNodes

--- src/elements/funkwhale/Funkwhale.wc.svelte
+++ src/elements/funkwhale/Funkwhale.wc.svelte
@@ -6,49 +6,49 @@
     IPackage,
     ITab,
     SelectCapacityUpdate,
-  } from "../../types";
-  import type { IProfile } from "../../types/Profile";
+  } from '../../types';
+  import type { IProfile } from '../../types/Profile';
 
   const deploymentStore = window.configs?.deploymentStore;
 
-  import VM, { Disk, Env } from "../../types/vm";
-  import deployFunkwhale from "../../utils/deployFunkwhale";
+  import VM, { Disk, Env } from '../../types/vm';
+  import deployFunkwhale from '../../utils/deployFunkwhale';
 
   // Components
-  import SelectProfile from "../../components/SelectProfile.svelte";
-  import Input from "../../components/Input.svelte";
-  import Tabs from "../../components/Tabs.svelte";
-  import SelectNodeId from "../../components/SelectNodeId.svelte";
-  import DeployBtn from "../../components/DeployBtn.svelte";
-  import Alert from "../../components/Alert.svelte";
-  import Modal from "../../components/DeploymentModal.svelte";
+  import SelectProfile from '../../components/SelectProfile.svelte';
+  import Input from '../../components/Input.svelte';
+  import Tabs from '../../components/Tabs.svelte';
+  import SelectNodeId from '../../components/SelectNodeId.svelte';
+  import DeployBtn from '../../components/DeployBtn.svelte';
+  import Alert from '../../components/Alert.svelte';
+  import Modal from '../../components/DeploymentModal.svelte';
 
-  import AlertDetailed from "../../components/AlertDetailed.svelte";
-  import hasEnoughBalance from "../../utils/hasEnoughBalance";
+  import AlertDetailed from '../../components/AlertDetailed.svelte';
+  import hasEnoughBalance from '../../utils/hasEnoughBalance';
   import validateName, {
     isInvalid,
     validateEmail,
     validatePassword,
-  } from "../../utils/validateName";
-  import { noActiveProfile } from "../../utils/message";
-  import rootFs from "../../utils/rootFs";
-  import Funkwhale from "../../types/funkwhale";
-  import SelectCapacity from "../../components/SelectCapacity.svelte";
-  import SelectGatewayNode from "../../components/SelectGatewayNode.svelte";
-  import type { GatewayNodes } from "../../utils/gatewayHelpers";
+  } from '../../utils/validateName';
+  import { noActiveProfile } from '../../utils/message';
+  import rootFs from '../../utils/rootFs';
+  import Funkwhale from '../../types/funkwhale';
+  import SelectCapacity from '../../components/SelectCapacity.svelte';
+  import SelectGatewayNode from '../../components/SelectGatewayNode.svelte';
+  import type { GatewayNodes } from '../../utils/gatewayHelpers';
 
   const data = new Funkwhale();
   data.disks = [new Disk()];
 
-  const tabs: ITab[] = [{ label: "Base", value: "base" }];
+  const tabs: ITab[] = [{ label: 'Base', value: 'base' }];
   let profile: IProfile;
 
-  let active: string = "base";
+  let active: string = 'base';
   let modalData: Object;
   let loading = false;
   let success = false;
   let failed = false;
-  let status: "valid" | "invalid";
+  let status: 'valid' | 'invalid';
 
   const nameField: IFormField = { label: "Name", placeholder: "Funkwhale Instance Name", symbol: "name", type: "text", validator: validateName, invalid: false }; // prettier-ignore
   const userNameField: IFormField = { label: "Username", placeholder: "Username will be used to access your profile", symbol: "username", type: "text", validator: validateName, invalid: false }; // prettier-ignore
@@ -58,9 +58,9 @@
 
   // define this solution packages
   const packages: IPackage[] = [
-    { name: "Minimum", cpu: 2, memory: 1024, diskSize: 50 },
-    { name: "Standard", cpu: 2, memory: 1024 * 2, diskSize: 100 },
-    { name: "Recommended", cpu: 4, memory: 1024 * 4, diskSize: 250 },
+    { name: 'Minimum', cpu: 2, memory: 1024, diskSize: 50 },
+    { name: 'Standard', cpu: 2, memory: 1024 * 2, diskSize: 100 },
+    { name: 'Recommended', cpu: 4, memory: 1024 * 4, diskSize: 250 },
   ];
   let selectCapacity = new SelectCapacityUpdate();
 
@@ -82,7 +82,7 @@
       failed = true;
       loading = false;
       message =
-        "No enough balance to execute! Transaction requires 2 TFT at least in your wallet.";
+        'No enough balance to execute! Transaction requires 2 TFT at least in your wallet.';
       return;
     }
 
@@ -94,7 +94,7 @@
       })
       .catch((err) => {
         failed = true;
-        message = typeof err === "string" ? err : err.message;
+        message = typeof err === 'string' ? err : err.message;
       })
       .finally(() => {
         loading = false;
@@ -108,7 +108,7 @@
   on:profile={({ detail }) => {
     profile = detail;
     if (detail) {
-      data.envs[0] = new Env(undefined, "SSH_KEY", detail.sshKey);
+      data.envs[0] = new Env(undefined, 'SSH_KEY', detail.sshKey);
     }
   }}
 />
@@ -129,8 +129,8 @@
     </p>
     <hr />
 
-    {#if loading || (logs !== null && logs.type === "Funkwhale")}
-      <Alert type="info" message={logs?.message ?? "Loading..."} />
+    {#if loading || (logs !== null && logs.type === 'Funkwhale')}
+      <Alert type="info" message={logs?.message ?? 'Loading...'} />
     {:else if !profile}
       <Alert type="info" message={noActiveProfile} />
     {:else if success}
@@ -140,66 +140,65 @@
         deployed={true}
       />
     {:else if failed}
-      <Alert type="danger" message={message || "Failed to deploy Funkwhale"} />
+      <Alert type="danger" message={message || 'Failed to deploy Funkwhale'} />
     {:else}
       <Tabs bind:active {tabs} />
-      {#if active === "base"}
-        <Input
-          bind:data={data.name}
-          bind:invalid={nameField.invalid}
-          field={nameField}
-        />
-        <Input
-          bind:data={data.adminUsername}
-          bind:invalid={userNameField.invalid}
-          field={userNameField}
-        />
-        <Input
-          bind:data={data.adminEmail}
-          bind:invalid={emailField.invalid}
-          field={emailField}
-        />
-        <Input
-          bind:data={data.adminPassword}
-          bind:invalid={passwordField.invalid}
-          field={passwordField}
-        />
 
-        <SelectCapacity
-          {packages}
-          selectedPackage={selectCapacity.selectedPackage}
-          cpu={data.cpu}
-          memory={data.memory}
-          diskSize={data.disks[0].size}
-          on:update={({ detail }) => {
-            selectCapacity = detail;
-            if (!detail.invalid) {
-              const { cpu, memory, diskSize } = detail.package;
-              data.cpu = cpu;
-              data.memory = memory;
-              data.disks[0].size = diskSize;
-            }
-          }}
-        />
-        <SelectGatewayNode bind:gateway bind:invalid />
+      <Input
+        bind:data={data.name}
+        bind:invalid={nameField.invalid}
+        field={nameField}
+      />
+      <Input
+        bind:data={data.adminUsername}
+        bind:invalid={userNameField.invalid}
+        field={userNameField}
+      />
+      <Input
+        bind:data={data.adminEmail}
+        bind:invalid={emailField.invalid}
+        field={emailField}
+      />
+      <Input
+        bind:data={data.adminPassword}
+        bind:invalid={passwordField.invalid}
+        field={passwordField}
+      />
 
-        <SelectNodeId
-          publicIp={false}
-          cpu={data.cpu}
-          memory={data.memory}
-          ssd={data.disks.reduce(
-            (total, disk) => total + disk.size,
-            rootFs(data.cpu, data.memory)
-          )}
-          bind:data={data.nodeId}
-          bind:nodeSelection={data.selection.type}
-          bind:status
-          filters={data.selection.filters}
-          {profile}
-          on:fetch={({ detail }) => (data.selection.nodes = detail)}
-          nodes={data.selection.nodes}
-        />
-      {/if}
+      <SelectCapacity
+        {packages}
+        selectedPackage={selectCapacity.selectedPackage}
+        cpu={data.cpu}
+        memory={data.memory}
+        diskSize={data.disks[0].size}
+        on:update={({ detail }) => {
+          selectCapacity = detail;
+          if (!detail.invalid) {
+            const { cpu, memory, diskSize } = detail.package;
+            data.cpu = cpu;
+            data.memory = memory;
+            data.disks[0].size = diskSize;
+          }
+        }}
+      />
+      <SelectGatewayNode bind:gateway bind:invalid />
+
+      <SelectNodeId
+        publicIp={false}
+        cpu={data.cpu}
+        memory={data.memory}
+        ssd={data.disks.reduce(
+          (total, disk) => total + disk.size,
+          rootFs(data.cpu, data.memory)
+        )}
+        bind:data={data.nodeId}
+        bind:nodeSelection={data.selection.type}
+        bind:status
+        filters={data.selection.filters}
+        {profile}
+        on:fetch={({ detail }) => (data.selection.nodes = detail)}
+        nodes={data.selection.nodes}
+      />
     {/if}
 
     <DeployBtn
@@ -224,5 +223,5 @@
 {/if}
 
 <style lang="scss" scoped>
-  @import url("https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css");
+  @import url('https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css');
 </style>
