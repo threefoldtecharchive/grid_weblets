cee5f44e84bbde6880eed8f03b058b304bace1e8
kassem <omarksm09@gmail.com>
2022-11-22 18:29:54 +0200

    add pingNode to Mattermost

--- src/utils/deployMattermost.ts
+++ src/utils/deployMattermost.ts
@@ -98,11 +98,15 @@ function _deployMatterMost(profile: IProfile, mattermost: Mattermost) {
 
   return deploy(profile, "Mattermost", name, async (grid) => {
     await checkVMExist(grid, "mattermost", name);
-
-    return grid.machines
-      .deploy(vms)
-      .then(() => grid.machines.getObj(name))
-      .then(([vm]) => vm);
+    try {
+      await grid.zos.pingNode({ nodeId: vm.node_id });
+      return grid.machines
+        .deploy(vms)
+        .then(() => grid.machines.getObj(name))
+        .then(([vm]) => vm);
+    } catch (error) {
+      throw error;
+    }
   });
 }
 
