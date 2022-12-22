8cde61fa7f4b7575474597cce67f037060fabd53
kassem <omarksm09@gmail.com>
2022-12-22 16:06:42 +0200

    linting

--- src/utils/getSignerObj.ts
+++ src/utils/getSignerObj.ts
@@ -1,7 +1,7 @@
 // const { web3Accounts, web3Enable, web3FromSource } =
 //   window.configs?.polkadot ?? {};
 
-export default async function getSignerObj(name: string) {
+export default async function getSignerObj() {
   // await web3Enable(name);
   // const [{ address, meta: { source } }] = await web3Accounts(); // prettier-ignore
   // const { signer } = await web3FromSource(source);
