cc9c5d7a942ebad005efe79a25c09837950e4ee2
MohamedElmdary <engm5081@gmail.com>
2023-01-19 11:21:48 +0200

    Fix colors & typo

--- src/elements/profiles/Profiles.wc.svelte
+++ src/elements/profiles/Profiles.wc.svelte
@@ -215,7 +215,6 @@
       </div>
       <button
         class="button is-primary mr-2 is-small"
-        class:is-outlined={!migrateMode}
         on:click={function () {
           migrateMode = !migrateMode;
           this.blur();
@@ -365,8 +364,8 @@
         <div class="field mt-2">
           <label class="label mb-0" for="ssh">Public SSH Key</label>
           <p class="mb-2 is-size-6 has-text-grey">
-            SSH Keys are used to authenticate you to the Mastodon instance for management purposes. If you don't have an
-            SSH Key or are not familiar, we can generate one for you.
+            SSH Keys are used to authenticate you to the deployment instance for management purposes. If you don't have
+            an SSH Key or are not familiar, we can generate one for you.
           </p>
           <div class="is-flex is-justify-content-space-between">
             <div class="control is-flex-grow-1 mr-3" class:is-loading={sshLoading}>
@@ -422,4 +421,66 @@
 <style lang="scss" scoped>
   @import url("https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css");
   @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css");
+
+  strong {
+    color: inherit !important;
+  }
+
+  .button.is-primary {
+    background-color: #6261fb;
+  }
+
+  .button.is-primary:hover {
+    background-color: #4747f0;
+  }
+
+  .button.is-primary.is-outlined {
+    border-color: #6261fb;
+    color: #6261fb;
+  }
+
+  .button.is-primary.is-outlined:hover,
+  .button.is-primary.is-outlined:active,
+  .button.is-primary.is-outlined:focus {
+    color: #fff;
+    background-color: #6261fb;
+    border-color: transparent;
+  }
+
+  .button.is-primary.is-outlined:active,
+  .button.is-primary.is-outlined:focus {
+    box-shadow: 0 0 0 2px #9c9cf0;
+  }
+
+  .button.is-primary.is-outlined[disabled] {
+    border-color: #6261fb;
+  }
+
+  .button.is-primary.is-outlined.is-loading::after {
+    border-color: transparent transparent #6261fb #6261fb !important;
+  }
+
+  .input.is-success,
+  .textarea.is-success,
+  .select.is-success select,
+  .select.is-success:not(:hover)::after {
+    border-color: #6261fb !important;
+  }
+
+  .icon-text.has-text-success {
+    color: #6261fb !important;
+  }
+
+  .switch__input:checked + .slider {
+    background-color: #6261fb;
+  }
+
+  .notification.is-success.is-light {
+    background-color: #efeef0;
+    color: #6261fb;
+  }
+
+  .button.is-primary[disabled] {
+    background-color: #6261fb;
+  }
 </style>
