0d58959a943979b3e4ac7ebc6639b122f4977689
kassem <omarksm09@gmail.com>
2023-01-30 17:59:07 +0200

    modify wp type, add password generator

--- src/types/wp.ts
+++ src/types/wp.ts
@@ -1,9 +1,11 @@
+import { v4 } from "uuid";
 import VM from "./vm";
-
-export default class WP extends VM {
+import generatePassword from "../utils/generatePassword";
+export default class Wordpress extends VM {
+  public id = v4().split("-")[0];
   public name = `WP${this.id}`;
-  public username = "admin";
-  public password = "password";
-  public email = "";
+  public adminEmail = "";
+  public adminUsername = "admin";
+  public adminPassword = generatePassword(length); // prettier-ignore
   public domain = "";
 }
