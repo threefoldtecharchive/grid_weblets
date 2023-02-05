6dd3c7a700c61d34e33669ee31ba26a4f0509bbd
kassem <omarksm09@gmail.com>
2023-02-05 16:57:44 +0200

    reorganize imports

--- src/elements/wordpress/wordpress.wc.svelte
+++ src/elements/wordpress/wordpress.wc.svelte
@@ -1,30 +1,31 @@
 <svelte:options tag="tf-wordpress" />
 
 <script lang="ts">
-  import { IFormField, IPackage, ITab, SelectCapacityUpdate } from "../../types";
-  import type { IProfile } from "../../types/Profile";
-  import { Disk, Env } from "../../types/vm";
   import Wordpress from "../../types/wordpress";
-  import validateName, { isInvalid, validateRequiredEmail, validateRequiredPassword } from "../../utils/validateName";
+  import type { IProfile } from "../../types/Profile";
+  import { IFormField, IPackage, ITab, SelectCapacityUpdate } from "../../types";
+  import deployWordpress from "../../utils/deployWordpress";
+  import { Disk } from "../../types/vm";
 
   // Components
   import SelectProfile from "../../components/SelectProfile.svelte";
-  import Alert from "../../components/Alert.svelte";
-  import { noActiveProfile } from "../../utils/message";
-  import Tabs from "../../components/Tabs.svelte";
-  import { display } from "../../utils/display";
   import Input from "../../components/Input.svelte";
-  import SelectCapacity from "../../components/SelectCapacity.svelte";
+  import Tabs from "../../components/Tabs.svelte";
+  import DeployBtn from "../../components/DeployBtn.svelte";
+  import Alert from "../../components/Alert.svelte";
+  import SelectNodeId from "../../components/SelectNodeId.svelte";
+  import Modal from "../../components/DeploymentModal.svelte";
+
+  //util
+  import hasEnoughBalance from "../../utils/hasEnoughBalance";
+  import validateName, { isInvalid, validateRequiredEmail, validateRequiredPassword } from "../../utils/validateName";
+  import { noActiveProfile } from "../../utils/message";
   import SelectGatewayNode from "../../components/SelectGatewayNode.svelte";
   import type { GatewayNodes } from "../../utils/gatewayHelpers";
-  import SelectNodeId from "../../components/SelectNodeId.svelte";
-  import rootFs from "../../utils/rootFs";
-  import DeployBtn from "../../components/DeployBtn.svelte";
-  import Modal from "../../components/DeploymentModal.svelte";
-  import deployWordpress from "../../utils/deployWordpress";
-  import hasEnoughBalance from "../../utils/hasEnoughBalance";
+  import SelectCapacity from "../../components/SelectCapacity.svelte";
   import normalizeDeploymentErrorMessage from "../../utils/normalizeDeploymentErrorMessage";
 
+  import { display } from "../../utils/display";
   let loading = false;
   let success = false;
   let failed = false;
@@ -142,45 +143,44 @@
       <Alert type="danger" {message} />
     {:else}
       <Tabs bind:active {tabs} />
-      <section style={display(active, "config")}>
-        {#each fields as field (field.symbol)}
-          {#if field.invalid !== undefined}
-            <Input bind:data={data[field.symbol]} bind:invalid={field.invalid} {field} />
-          {:else}
-            <Input bind:data={data[field.symbol]} {field} />
-          {/if}
-        {/each}
-        <SelectCapacity
-          {packages}
-          selectedPackage={selectCapacity.selectedPackage}
-          cpu={data.cpu}
-          memory={data.memory}
-          diskSize={data.diskSize}
-          on:update={({ detail }) => {
-            selectCapacity = detail;
-            if (!detail.invalid) {
-              const { cpu, memory, diskSize } = detail.package;
-              data.cpu = cpu;
-              data.memory = memory;
-              data.disks[0].size = diskSize;
-            }
-          }}
-        />
-        <SelectGatewayNode bind:gateway bind:invalid />
-        <SelectNodeId
-          cpu={data.cpu}
-          memory={data.memory}
-          publicIp={false}
-          ssd={data.diskSize + rootFs(data.cpu, data.memory)}
-          bind:data={data.nodeId}
-          bind:nodeSelection={data.selection.type}
-          bind:status
-          filters={data.selection.filters}
-          {profile}
-          on:fetch={({ detail }) => (data.selection.nodes = detail)}
-          nodes={data.selection.nodes}
-        />
-      </section>
+
+      {#each fields as field (field.symbol)}
+        {#if field.invalid !== undefined}
+          <Input bind:data={data[field.symbol]} bind:invalid={field.invalid} {field} />
+        {:else}
+          <Input bind:data={data[field.symbol]} {field} />
+        {/if}
+      {/each}
+      <SelectCapacity
+        {packages}
+        selectedPackage={selectCapacity.selectedPackage}
+        cpu={data.cpu}
+        memory={data.memory}
+        diskSize={data.diskSize}
+        on:update={({ detail }) => {
+          selectCapacity = detail;
+          if (!detail.invalid) {
+            const { cpu, memory, diskSize } = detail.package;
+            data.cpu = cpu;
+            data.memory = memory;
+            data.disks[0].size = diskSize;
+          }
+        }}
+      />
+      <SelectGatewayNode bind:gateway bind:invalid />
+      <SelectNodeId
+        cpu={data.cpu}
+        memory={data.memory}
+        publicIp={false}
+        ssd={data.disks.reduce((total, disk) => total + disk.size, 0)}
+        bind:data={data.nodeId}
+        bind:nodeSelection={data.selection.type}
+        bind:status
+        filters={data.selection.filters}
+        {profile}
+        on:fetch={({ detail }) => (data.selection.nodes = detail)}
+        nodes={data.selection.nodes}
+      />
     {/if}
     <DeployBtn
       {disabled}
