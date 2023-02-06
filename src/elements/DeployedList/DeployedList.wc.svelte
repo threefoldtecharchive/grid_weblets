7844701962f87c65f65bab1770ea78ed251f81e4
kassem <omarksm09@gmail.com>
2023-02-06 14:43:58 +0200

    add visit and Admin Panel buttons

--- src/elements/DeployedList/DeployedList.wc.svelte
+++ src/elements/DeployedList/DeployedList.wc.svelte
@@ -495,6 +495,30 @@
           disabled: () => removing !== null,
           loading: i => removing === rows[i].name,
         },
+        {
+          type: "warning",
+          label: "Visit",
+          click: (_, i) => {
+            const domain = rows[i].details.env.WP_URL;
+            window.open(domain, "_blank").focus();
+          },
+          disabled: i => {
+            const env = rows[i].details.env;
+            return !env || !env.WP_URL || removing !== null;
+          },
+        },
+        {
+          type: "success",
+          label: "Admin Panel",
+          click: (_, i) => {
+            const domain = rows[i].details.env.WP_URL;
+            window.open(domain + "/wp-admin", "_blank").focus();
+          },
+          disabled: i => {
+            const env = rows[i].details.env;
+            return !env || !env.DOMAIN_NAME || removing !== null;
+          },
+        },
       ],
     },
     {
