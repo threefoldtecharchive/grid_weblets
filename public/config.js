d6a9f81b1998a96456ed8c031c98b191911528b9
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-02-23 16:35:25 +0200

    add the env var needed for running in development

---
+++ public/config.js
@@ -0,0 +1,12 @@
+window.env = {
+  NETWORK: "dev",
+  GRAPHQL_URL: "https://graphql.dev.grid.tf/graphql",
+  GRIDPROXY_URL: "https://gridproxy.dev.grid.tf",
+  SUBSTRATE_URL: "wss://tfchain.dev.grid.tf/ws",
+  ACTIVATION_SERVICE_URL: "https://activation.dev.grid.tf",
+  RELAY_DOMAIN: "relay.dev.grid.tf",
+  BRIDGE_TFT_ADDRESS: "GDHJP6TF3UXYXTNEZ2P36J5FH7W4BJJQ4AYYAXC66I2Q2AH5B6O6BCFG",
+  STELLAR_NETWORK: "test",
+  STELLAR_HORIZON_URL: "https://horizon-testnet.stellar.org",
+  TFT_ASSET_ISSUER: "GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3",
+};
