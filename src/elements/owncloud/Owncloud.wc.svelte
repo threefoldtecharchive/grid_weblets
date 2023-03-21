34d99a2012487b2ae1dc47ef31bb60d8a1aa52cc
MohamedElmdary <engm5081@gmail.com>
2023-03-21 10:52:52 +0200

    Apply solution for Owncloud

--- src/elements/owncloud/Owncloud.wc.svelte
+++ src/elements/owncloud/Owncloud.wc.svelte
@@ -165,6 +165,12 @@
   }
 
   $: logs = $currentDeployment;
+
+  $: showLogs = loading || (logs !== null && logs.type === "Owncloud");
+  $: showNoProfile = !showLogs && !profile;
+  $: showSuccess = !showLogs && !showNoProfile && success;
+  $: showFailed = !showLogs && !showNoProfile && failed;
+  $: showContent = !showLogs && !showNoProfile && !showSuccess && !showFailed;
 </script>
 
 <SelectProfile
@@ -188,15 +194,23 @@
     </p>
     <hr />
 
-    {#if loading || (logs !== null && logs.type === "VM")}
+    <div style:display={showLogs ? "block" : "none"}>
       <Alert type="info" message={logs?.message ?? "Loading..."} />
-    {:else if !profile}
+    </div>
+
+    <div style:display={showNoProfile ? "block" : "none"}>
       <Alert type="info" message={noActiveProfile} />
-    {:else if success}
-      <Alert type="success" message="Successfully deployed owncloud." deployed={true} />
-    {:else if failed}
+    </div>
+
+    <div style:display={showSuccess ? "block" : "none"}>
+      <Alert type="success" message="Successfully Deployed Wordpress." deployed={true} />
+    </div>
+
+    <div style:display={showFailed ? "block" : "none"}>
       <Alert type="danger" {message} />
-    {:else}
+    </div>
+
+    <div style:display={showContent ? "block" : "none"}>
       <Tabs bind:active {tabs} />
 
       <section style={display(active, "base")}>
@@ -271,7 +285,7 @@
           {/if}
         {/each}
       </section>
-    {/if}
+    </div>
 
     <DeployBtn
       {disabled}
