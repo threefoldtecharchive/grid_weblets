7d757e417c828c0b51c8559486caf9eac2c1aa29
kassem <omarksm09@gmail.com>
2023-03-22 17:11:44 +0200

    handle deleting gw

--- src/utils/deleteDeployment.ts
+++ src/utils/deleteDeployment.ts
@@ -1,3 +1,4 @@
+import type { GridClient } from "grid3_client";
 import type { IStore } from "./../stores/currentDeployment";
 import { getUniqueDomainName } from "./gatewayHelpers";
 
@@ -6,6 +7,18 @@ interface IConfig {
   storeSecret: string;
   networkEnv: string;
 }
+
+const solutionWithGW: IStore["type"][] = [
+  "Discourse",
+  "Funkwhale",
+  "Mastodon",
+  "Mattermost",
+  "Owncloud",
+  "Peertube",
+  "Subsquid",
+  "Taiga",
+  "Wordpress",
+];
 
 export default async function deleteDeployment(
   configs: IConfig,
@@ -26,30 +39,63 @@ export default async function deleteDeployment(
     activationURL: window.env.ACTIVATION_SERVICE_URL,
     relayURL: window.env.RELAY_DOMAIN,
   });
+  const hasGw = solutionWithGW.includes(type);
+  let defaultNameSpace = false;
 
   // remove deployment in namespace `type`
-  console.log({ grid });
   await grid.connect();
-  const obj = await _deleteDeployments(grid, name, configs, type, key);
+  const obj = await _deleteDeployments(grid, name, configs, type, key, defaultNameSpace, hasGw);
 
-  console.log({ obj });
-  if (obj["deleted"].length) {
+  console.log(obj);
+  if (obj.length) {
     return obj;
   }
 
   // remove deployments (vm, gw) in default namespace ``
-  grid.clientOptions.projectName = "";
-  await grid._connect();
-  console.log({ grid });
-  return await _deleteDeployments(grid, name, configs, "", key);
+  defaultNameSpace = true;
+  return await _deleteDeployments(grid, name, configs, type, key, defaultNameSpace, hasGw);
 }
 
-async function _deleteDeployments(grid, name, configs, type, key) {
+async function _deleteDeployments(grid, name, configs, type, key, defaultNameSpace = false, hasGw) {
   const domainName = await getUniqueDomainName(configs, name, type);
+
+  if (defaultNameSpace) {
+    grid.clientOptions.projectName = "";
+    await grid._connect();
+  }
+
   if (type === "Qvm") {
     const qvm = await grid.machines.getObj(name);
     await grid.qsfs_zdbs.delete({ name: qvm[0].mounts[0].name });
   }
-  await grid.gateway.delete_name({ name: domainName });
-  return await grid[key].delete({ name });
+
+  const deleted = await grid[key].delete({ name });
+  if (!hasGw || defaultNameSpace) return deleted;
+
+  const deletedgw = await _deleteDeploymentsGw(grid, domainName);
+  console.log(deleted);
+  if (!deletedgw) throw new Error("Can't delete gateway contracts with name " + domainName);
+
+  return deleted;
+}
+
+async function _deleteDeploymentsGw(grid: GridClient, domainName: string) {
+  let deleted = await deleteGw(grid, grid.clientOptions.projectName, domainName);
+  if (deleted) return deleted;
+
+  deleted = await deleteGw(grid, "GatewayName", domainName);
+  if (deleted) return deleted;
+
+  deleted = await deleteGw(grid, "", domainName);
+  if (deleted) return deleted;
+
+  return null;
+}
+
+async function deleteGw(grid: GridClient, namespace: string, domainName: string) {
+  grid.clientOptions.projectName = namespace;
+  await grid._connect();
+  const { deleted } = await grid.gateway.delete_name({ name: domainName });
+  if (deleted.length) return deleted;
+  else return null;
 }
